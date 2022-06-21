994225e1e4619da7c205995b1c1fcf5b9e3b5627
clayton wright <claytonwright@claytons-MacBook-Pro.local>
2022-06-21 10:13:55 -0400

    completed, all pass

--- main.js
+++ main.js
@@ -1,16 +1,63 @@
 // ┌─────────────────┐
 // │ Your code here! │
 // └─────────────────┘
+function createUser(firstName, lastName){
+    let user = {
+        firstName: firstName,
+        lastName: lastName
+    }
+    return user
+}
+
+function setAge(user, age){
+    user.age = age
+    return user
+}
+
+function incrementAge(user){
+    user.age = user.age + 1
+    return user
+}
 
 
+function fixCar(car){
+    car.needsMaintenance = false
+    return car
+}
 
-
-
-
-
-
-
-
+function addGrades(user, grades){
+    grades.forEach(grades => {
+        user.grades.push(grades)
+    })
+    return user
+}
+function getDataType(object,prop){
+    return typeof object[prop]
+}
+function addTodo(items, item){
+    items.push(item)
+    return items
+}
+function addSong (playlist,song){
+    playlist.songs.push(song)
+    playlist.duration = playlist.duration + song.duration
+    return playlist
+}
+function updateReportCard (reportCard,newGrade){
+    reportCard.grades.push(newGrade)
+    reportCard.averageGrade = 0
+    reportCard.grades.forEach(grade => {
+        reportCard.averageGrade = reportCard.averageGrade+grade
+    })
+    reportCard.averageGrade = reportCard.averageGrade/reportCard.grades.length
+    if (newGrade < reportCard.lowestGrade){
+        reportCard.lowestGrade = newGrade
+    }
+    if (newGrade > reportCard.highestGrade){
+        reportCard.highestGrade = newGrade
+    }
+    return reportCard
+}
 // ┌─────────────────────────────────────────────────────────────────────────┐
 // │ Code used for testing. Do not modify!                                   │                         
 // │                                                                         │
